bd804eca96c81b1ec2300bdc7aec7d7304b954f3
vlavasa <ego@vlavasa.cz>
2026-09-14 21:22:34 +0200

    non breaking space in description

--- configs/user.config.ts
+++ configs/user.config.ts
@@ -3,7 +3,7 @@ import type { UserConfig } from "../src/site.config";
 const userConfig: UserConfig = {
   title: "Lipi4k",
   description:
-    "An actively maintained fork of Lipi, a typography-first Astro template.",
+    "An actively maintained fork of Lipi, a typography-first Astro template.",
 
   url: "https://vlavasa.github.io",
   author: "John Doe",
